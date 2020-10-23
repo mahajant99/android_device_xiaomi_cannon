f10ffc44aa4be4c166dd6f2a094d975bab8212c5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-23 23:48:40 +0200

    cannong: Reboot to recovery if init fails
    Change-Id: I1d8f38620caa7b003911ff30f69ed7973c100e6d
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
